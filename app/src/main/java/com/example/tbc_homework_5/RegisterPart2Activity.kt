b4975592a758afc3de463ac008576ce316b6661a
Tengiz <Tengiz152@gmail.com>
2022-06-29 03:02:05 +0400

    created layout design for the app

---
+++ app/src/main/java/com/example/tbc_homework_5/RegisterPart2Activity.kt
@@ -0,0 +1,11 @@
+package com.example.tbc_homework_5
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterPart2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register_part_2)
+    }
+}
