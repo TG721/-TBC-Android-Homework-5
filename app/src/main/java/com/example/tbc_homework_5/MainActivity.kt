3b5820f8b7394df6daa52bca69a30af6fc0854c3
Tengiz <Tengiz152@gmail.com>
2022-06-29 03:49:36 +0400

    Added intents to switch between layouts

--- app/src/main/java/com/example/tbc_homework_5/MainActivity.kt
+++ app/src/main/java/com/example/tbc_homework_5/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.tbc_homework_5
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tbc_homework_5.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fromMainLoginButton.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
+        }
+        binding.fromMainRegisterButton.setOnClickListener{
+            val intent=Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/tbc_homework_5/RegisterActivity.kt
+++ app/src/main/java/com/example/tbc_homework_5/RegisterActivity.kt
@@ -1,11 +1,20 @@
 package com.example.tbc_homework_5
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class RegisterActivity : AppCompatActivity(){
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register)
+
+        val  nextButton: Button = findViewById(R.id.registerNextButton)
+        nextButton.setOnClickListener {
+            val intent= Intent(this, RegisterPart2Activity::class.java)
+            startActivity(intent)
+        }
     }
 }
