8c9f0a30923dd947d9a43909485f5a158c643910
Tengiz <Tengiz152@gmail.com>
2022-06-29 06:24:22 +0400

    added firebase login part

--- app/src/main/java/com/example/tbc_homework_5/LoginActivity.kt
+++ app/src/main/java/com/example/tbc_homework_5/LoginActivity.kt
@@ -1,11 +1,84 @@
 package com.example.tbc_homework_5
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.tbc_homework_5.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+
+
+
+//FirebaseAuth
+private lateinit var firebaseAuth : FirebaseAuth
+private var email = ""
+private var password = ""
 
 class LoginActivity : AppCompatActivity(){
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+
+        //init firebaseAuth
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        //button click handling
+        val  logInButton: Button = findViewById(R.id.loginButton)
+        validateData()
+
+
     }
+
+    private fun validateData() {
+        //get data
+    email = (findViewById(R.id.loginEmail) as EditText).text.toString().trim()
+    password = (findViewById(R.id.loginPassword) as EditText).text.toString().trim()
+
+        //validate data
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            //invalid email format
+            (findViewById(R.id.emailError) as TextView).error = "Invalid email format"
+
+        }
+        else if (password.isEmpty()){
+            //no password was entered
+            (findViewById(R.id.passwordError) as TextView).error = "Please enter password"
+        }
+        else {
+            //data is validated, we begin login
+            firebaseLogin()
+        }
+    }
+
+    private fun firebaseLogin() {
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                //login success
+
+                //get user info
+                val firebaseUser  = firebaseAuth.currentUser
+                val email = firebaseUser!!.email
+                Toast.makeText(this, "Logged in as $email", Toast.LENGTH_SHORT).show()
+
+                //go to LoggedIn/profile activity
+                startActivity(Intent(this, LoggedInActivity::class.java))
+                finish()
+
+
+            }
+            .addOnFailureListener{ e->
+                //login failed
+                Toast.makeText(this, "Login failed due to ${e.message}", Toast.LENGTH_SHORT).show()
+
+
+            }
+    }
+
+
 }
--- app/src/main/java/com/example/tbc_homework_5/MainActivity.kt
+++ app/src/main/java/com/example/tbc_homework_5/MainActivity.kt
@@ -4,6 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tbc_homework_5.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+
+//FirebaseAuth
+private lateinit var firebaseAuth : FirebaseAuth
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +18,11 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //init firebaseAuth
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
+
+        //buttons click handling
         binding.fromMainLoginButton.setOnClickListener{
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
@@ -23,5 +32,19 @@ class MainActivity : AppCompatActivity() {
             val intent=Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun checkUser() {
+            //if user is already logged in
+            //go to LoggedIn/profile activity
+
+            //get current user
+            val firebaseUser = firebaseAuth.currentUser
+            if (firebaseUser !=null){
+                //user is already logged in
+                startActivity(Intent(this, LoggedInActivity::class.java))
+                finish()
+            }
+
     }
 }
