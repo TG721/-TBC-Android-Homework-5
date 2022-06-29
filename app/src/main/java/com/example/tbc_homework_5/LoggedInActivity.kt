635359fb6f4d95e79e75300ad329b1eef9519477
Tengiz <Tengiz152@gmail.com>
2022-06-29 07:43:33 +0400

    finished working on loggedIn activity

--- app/src/main/java/com/example/tbc_homework_5/LoggedInActivity.kt
+++ app/src/main/java/com/example/tbc_homework_5/LoggedInActivity.kt
@@ -1,11 +1,54 @@
 package com.example.tbc_homework_5
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class LoggedInActivity: AppCompatActivity() {
+
+    //Firebase Auth
+    private lateinit var firebaseAuth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.logged_in)
+
+        //init firebase auth
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
+
+        //handle logout button
+        (findViewById(R.id.log_out) as Button).setOnClickListener {
+            firebaseAuth.signOut()
+            checkUser()
+        }
+    }
+
+
+
+
+    private fun checkUser() {
+        //if user is already logged in
+        //go to LoggedIn/profile activity
+
+        //get current user
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser !=null){
+            //user is already logged in
+            //get user info
+            var email = firebaseUser.email
+            //set textview to user email
+            (findViewById(R.id.loginInfo) as TextView).text=email
+        }
+        else {
+            //user is null and not logged in, go to main page
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+
+        }
     }
 }
--- app/src/main/java/com/example/tbc_homework_5/RegisterPart2Activity.kt
+++ app/src/main/java/com/example/tbc_homework_5/RegisterPart2Activity.kt
@@ -1,11 +1,19 @@
 package com.example.tbc_homework_5
 
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class RegisterPart2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register_part_2)
+
+        (findViewById(R.id.singUpButton) as Button).setOnClickListener {
+            startActivity(Intent(this, LoggedInActivity::class.java))
+            finish()
+        }
+
     }
 }
