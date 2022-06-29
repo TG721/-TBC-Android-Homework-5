3dc31ea17143bbf4e02bbe1dc10b653761eb74e9
Tengiz <Tengiz152@gmail.com>
2022-06-29 04:51:04 +0400

    added "profile/login info" layout

---
+++ app/src/main/java/com/example/tbc_homework_5/LoggedInActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tbc_homework_5
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoggedInActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.logged_in)
+    }
+}
